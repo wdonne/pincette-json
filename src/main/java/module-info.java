f7433812b578ea80db574add793fad0cc28a740d
Werner Donné <werner.donne@pincette.biz>
2021-09-09 13:47:39 +0200

    Update dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module net.pincette.json {
   requires java.xml;
   requires net.pincette.common;
   requires com.fasterxml.jackson.databind;
-  requires jslt;
+  requires com.schibsted.spt.data.jslt;
   requires java.logging;
   requires com.fasterxml.jackson.dataformat.yaml;
   exports net.pincette.json;
